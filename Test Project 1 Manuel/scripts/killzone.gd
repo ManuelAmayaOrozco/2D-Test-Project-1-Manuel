468d739ebc02a2a9c6d762c58c915856ee0142df
ManuelAmayaOrozco <127980333+ManuelAmayaOrozco@users.noreply.github.com>
2025-10-03 13:10:24 +0200

    Score

--- Test Project 1 Manuel/scripts/killzone.gd
+++ Test Project 1 Manuel/scripts/killzone.gd
@@ -6,6 +6,9 @@ func _on_body_entered(body):
 	print("Oops you are dead")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
+	if body.has_method("die"):
+		Engine.time_scale = 0.5
+		body.die()
 	timer.start()
 
 func _on_timer_timeout():
